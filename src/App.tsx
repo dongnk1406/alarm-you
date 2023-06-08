59903749d27fdba875ac7b1532f1c7ac314f2f46
Dong <tryourbest146@gmail.com>
2023-06-08 16:43:47 +0700

    Chore: Add react native gift chat

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import {ThemeProvider} from '@shopify/restyle';
-import React, {useState} from 'react';
-import {SafeAreaView, ScrollView, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  LayoutAnimation,
+  Platform,
+  SafeAreaView,
+  ScrollView,
+  Switch,
+  Text,
+  UIManager,
+  View,
+} from 'react-native';
 import Config from 'react-native-config';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 import {darkTheme, lightTheme} from 'src/theme';
@@ -8,6 +17,15 @@ import {StyledBox, StyledImage, StyledText} from 'src/components';
 
 function App(): JSX.Element {
   const [darkMode, setDarkMode] = useState(false);
+  const [showMap, setShowMap] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      if (UIManager.setLayoutAnimationEnabledExperimental) {
+        UIManager.setLayoutAnimationEnabledExperimental(true);
+      }
+    }
+  }, []);
 
   return (
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
@@ -18,21 +36,32 @@ function App(): JSX.Element {
           }}
           style={{width: '100%', height: 300}}
         />
-        <MapView
-          provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-          style={{width: '100%', height: 300}}
-          region={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.0121,
+        <Switch
+          value={showMap}
+          onValueChange={value => {
+            LayoutAnimation.easeInEaseOut();
+            setShowMap(value);
           }}
         />
+        {showMap && (
+          <MapView
+            provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+            style={{width: '100%', height: 300}}
+            region={{
+              latitude: 37.78825,
+              longitude: -122.4324,
+              latitudeDelta: 0.015,
+              longitudeDelta: 0.0121,
+            }}
+          />
+        )}
         <StyledBox
           accessible={true}
           accessibilityLabel="A custom accessible view"
           accessibilityHint="Tap to perform an action.">
-          <StyledText variant="body" selectable>Hello ab da ajksf afnsoi fnsafnd</StyledText>
+          <StyledText variant="body" selectable>
+            Hello ab da ajksf afnsoi fnsafnd
+          </StyledText>
         </StyledBox>
       </ScrollView>
       {Config.MODE !== 'LIVE' && (
