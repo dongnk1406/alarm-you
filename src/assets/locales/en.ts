131c08013b8a9ccb966bb8c41ded9ff9b25d8624
Dong <tryourbest146@gmail.com>
2023-06-17 17:15:11 +0700

    Feat: Config multiple language

---
+++ src/assets/locales/en.ts
@@ -0,0 +1,18 @@
+const en = {
+  common: {
+    defaultLanguage: 'English',
+    close: 'Close',
+    noData: 'No data',
+    cancel: 'Cancel',
+    confirm: 'Confirm',
+    complete: 'Complete',
+    map: 'Map',
+    back: 'Back',
+    disconnect: 'Disconnect',
+    disconnectDescription: 'You lót the internet connection',
+  },
+};
+
+export type TTranslations = typeof en;
+
+export default en;
---
+++ src/assets/locales/vn.ts
@@ -0,0 +1,18 @@
+import {TTranslations} from './en';
+
+const vn: TTranslations = {
+  common: {
+    defaultLanguage: 'Việt Nam',
+    close: 'Đóng',
+    noData: 'Không có dữ liệu',
+    cancel: 'Hủy',
+    confirm: 'Xác nhận',
+    complete: 'Hoàn thành',
+    map: 'Bản đồ',
+    back: 'Quay lại',
+    disconnect: 'Ngắt kết nối',
+    disconnectDescription: 'Kết nối của bạn bị gián đoạn',
+  },
+};
+
+export default vn;
