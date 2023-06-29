65f84c274b6bc8d6fac5a59949efe6702c1a0820
Dong <tryourbest146@gmail.com>
2023-06-29 13:43:13 +0700

    feat: add husky format code

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,38 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  env: {
+    node: true,
+    es2021: true,
+    'react-native/react-native': true,
+  },
+  extends: ['@react-native-community'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-native'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-shadow': ['off'],
+        '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/no-empty-interface': [
+          'error',
+          {
+            allowSingleExtends: false,
+          },
+        ],
+        'react/no-unstable-nested-components': ['off', {allowAsProps: true}],
+        'react-hooks/exhaustive-deps': 'warn',
+        'no-shadow': 'off',
+        'no-undef': 'off',
+        'no-console': 1,
+      },
+    },
+  ],
 };
