76eb83903f28e009540954e1856c7d1fdecb578a
Dong <tryourbest146@gmail.com>
2023-08-14 08:42:42 +0700

    feat: add common components

--- src/containers/SignInScreen.tsx
+++ src/containers/SignInScreen.tsx
@@ -1,5 +1,8 @@
-import React, {useState} from 'react';
+import {yupResolver} from '@hookform/resolvers/yup';
+import React, {useEffect} from 'react';
+import {Controller, useForm} from 'react-hook-form';
 import {
+  LayoutAnimation,
   SafeAreaView,
   Text,
   TextInput,
@@ -9,45 +12,138 @@ import {
 import uuid from 'react-native-uuid';
 import {useAppDispatch} from 'src/redux/hooks';
 import {setSignIn} from 'src/redux/slices';
+import * as yup from 'yup';
 
+type IFormData = {
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
 const SignInScreen = () => {
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
   const dispatch = useAppDispatch();
 
-  const handleSubmit = async () => {
+  const signInSchema = yup
+    .object({
+      email: yup.string().required().email('can email'),
+      password: yup.string().required().max(10),
+      confirmPassword: yup
+        .string()
+        .required()
+        .oneOf([yup.ref('password'), ''], 'Need to match'),
+    })
+    .required();
+
+  const {
+    control,
+    handleSubmit,
+    formState: {errors, isValid},
+    watch,
+    reset,
+    resetField,
+    setValue,
+    getValues,
+    clearErrors,
+  } = useForm<IFormData>({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    mode: 'onSubmit',
+    resolver: yupResolver(signInSchema),
+  });
+
+  const onSubmit = async (form: IFormData) => {
     const token = uuid.v4().toString();
     dispatch(setSignIn(token));
   };
 
+  const onError = () => {};
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <View style={{flex: 1, paddingHorizontal: 16}}>
-        <Text>Email</Text>
-        <TextInput
-          value={email}
-          onChangeText={setEmail}
-          autoCapitalize="none"
-          style={{
-            borderWidth: 1,
-            borderRadius: 8,
-            borderColor: 'gray',
-            padding: 8,
+        <Text style={{color: 'black'}}>Email</Text>
+        <Controller
+          control={control}
+          rules={{
+            required: true,
           }}
+          name="email"
+          render={({field: {value, onChange, onBlur}}) => (
+            <TextInput
+              value={value}
+              onChangeText={onChange}
+              autoCapitalize="none"
+              style={{
+                borderWidth: 1,
+                borderRadius: 8,
+                borderColor: 'gray',
+                padding: 8,
+                color: 'black',
+              }}
+            />
+          )}
         />
-        <Text>Password</Text>
-        <TextInput
-          value={password}
-          onChangeText={setPassword}
-          autoCapitalize="none"
-          style={{
-            borderWidth: 1,
-            borderRadius: 8,
-            borderColor: 'gray',
-            padding: 8,
+        {errors.email && (
+          <Text style={{color: 'red'}}>{errors.email.message}</Text>
+        )}
+
+        <Text style={{color: 'black'}}>Password</Text>
+        <Controller
+          control={control}
+          rules={{
+            required: true,
           }}
+          name="password"
+          render={({field: {value, onChange, onBlur}}) => (
+            <TextInput
+              value={value}
+              onChangeText={onChange}
+              autoCapitalize="none"
+              style={{
+                borderWidth: 1,
+                borderRadius: 8,
+                borderColor: 'gray',
+                padding: 8,
+                color: 'black',
+              }}
+              placeholderTextColor={'grey'}
+            />
+          )}
         />
+        {errors.password && (
+          <Text style={{color: 'red'}}>{errors.password.message}</Text>
+        )}
+
+        <Text style={{color: 'black'}}>Confirm Password</Text>
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          name="confirmPassword"
+          render={({field: {value, onChange, onBlur}}) => (
+            <TextInput
+              value={value}
+              onChangeText={onChange}
+              autoCapitalize="none"
+              style={{
+                borderWidth: 1,
+                borderRadius: 8,
+                borderColor: 'gray',
+                padding: 8,
+                color: 'black',
+              }}
+              placeholderTextColor={'grey'}
+            />
+          )}
+        />
+        {errors.confirmPassword && (
+          <Text style={{color: 'red'}}>{errors.confirmPassword.message}</Text>
+        )}
+
         <TouchableOpacity
+          disabled={!isValid}
           style={{
             backgroundColor: 'orange',
             padding: 8,
@@ -55,8 +151,9 @@ const SignInScreen = () => {
             marginTop: 12,
             justifyContent: 'center',
             alignItems: 'center',
+            opacity: !isValid ? 0.5 : 1,
           }}
-          onPress={handleSubmit}>
+          onPress={handleSubmit(onSubmit)}>
           <Text>Submit</Text>
         </TouchableOpacity>
       </View>
